f653442921c4a4ce3b885a3449e5da8dcc3bf399
Vivek Kumar Yadav <vivekkumaryadav297@gmail.com>
2023-12-07 19:11:41 -0600

    78. Subsets

--- src/Q78Subsets/SubsetsBackTrack.java
+++ src/Q78Subsets/SubsetsBackTrack.java
@@ -13,8 +13,19 @@ package Q78Subsets;
 import java.util.ArrayList;
 import java.util.List;
 
+// 78. Subsets
+// Given an integer array nums of unique elements, return all possible
+// subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+//Example 1:
+//
+//        Input: nums = [1,2,3]
+//        Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Time complexity : O(2^n)
+// Space complexity: O(n*2^n)
 public class SubsetsBackTrack {
-    public List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         int index = 0;
         backTrack(nums, new ArrayList<>(), list, index);
@@ -34,5 +45,9 @@ public class SubsetsBackTrack {
             // back track
             tempList.remove(tempList.size()-1);
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subsets(new int[]{1, 2, 3}));
     }
 }
